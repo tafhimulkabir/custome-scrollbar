daff1ed7810e9e8ac95a10e02c4176cf74af0bbb
Tafhimul kabir <tafhimulkabir@gmail.com>
2023-02-27 20:45:32 +0600

    select scrollbar elements

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,11 +1,14 @@
 class CustomScrollbar {
 
     /**
-     * Constryusctor 
+     * Constructor 
      */
-    constructor()
+    constructor(selector)
     {
-        
+        this.appScrollbar = document.querySelector(selector);                                   // Select scrollbar container.
+        this.appScrollbarTrack = document.querySelector('#app__scrollbar__track');              // Select scrollbar track.
+        this.appScrollbarThumb = document.querySelector('.app__scrollbar__thumb');              // Select scrollbar thumb.
+        this.appScrollableContent = document.querySelector('.app__scrollable__content');        // Select scrollable content container.
     }
 
 }
