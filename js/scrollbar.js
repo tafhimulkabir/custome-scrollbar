6cd5887ab043a5577361a8c71e0e26f5e7c44c3a
Tafhimul kabir <tafhimulkabir@gmail.com>
2023-02-28 20:26:28 +0600

    updated comment

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,14 +1,37 @@
+/**
+ * CustomScrollbar is a class that creates a custom scrollbar for a given element.
+ * It listens to various events such as scroll, mouse down, mouse up, wheel, and resize
+ * to update the position and size of the scrollbar elements.
+ * 
+ * @param {string} selector - A CSS selector that identifies the element to add a custom scrollbar to.
+ * @returns {void}
+ */
 class CustomScrollbar {
 
     /**
-     * Constructor 
+     * This is the constructor function that initializes a custom scrollbar functionality 
+     * for an element selected by a provided selector. 
      */
     constructor(selector)
     {
+        // Select the necessary DOM elements.
         this.appScrollbar = document.querySelector(selector);                                   // Select scrollbar container.
         this.appScrollbarTrack = document.querySelector('#app__scrollbar__track');              // Select scrollbar track.
         this.appScrollbarThumb = document.querySelector('.app__scrollbar__thumb');              // Select scrollbar thumb.
         this.appScrollableContent = document.querySelector('.app__scrollable__content');        // Select scrollable content container.
+
+        // Add event listeners to the content, thumb, and scrollbar elements
+
     }
 
+    /**
+     * handleScroll is a function that updates the position of the thumb element
+     * based on the current scroll position of the content element.
+     * 
+     * @returns {void}
+     */
+    handleScroll()
+    {
+        const scrollPercentage = (this.content.scrollTop / (this.content.scrollHeight - this.content.clientHeight)) * 100;
+    }
 }
